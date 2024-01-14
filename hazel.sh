4dee57e229291cfe36c4a752da1c55dc1b37469e
nvz <corms-optics-0e@icloud.com>
2024-01-14 23:18:01 +0100

    auto-commit from nvz@M1-NVZ on Sun Jan 14 23:18:01 CET 2024

--- hazel.sh
+++ hazel.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-PYTHON=`which python3`
+PYTHON=$(which python3)
 ${PYTHON} ~/.config/little-snitch/importer.py $1
 
 message="auto-commit from $USER@$(hostname -s) on $(date)"
-GIT=`which git`
+GIT=$(which git)
 REPO_DIR="/Users/nvz/.config/little-snitch"
 cd ${REPO_DIR}
 ${GIT} add --all .
 ${GIT} commit -m "$message"
 
-gitPush=$(${GIT} push -vvv git@github.com:voisinicolas/ls-rules.git master 2>&1)
+gitPush=$(${GIT} push -vvv git@github.com:voisinicolas/ls-rules.git main 2>&1)
 echo "$gitPush"
